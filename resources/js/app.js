dd3eaa31ecb4047886498cfb53516e08a11ffc7f
IsoqulovBekzod <isoqulovbekzod6@gmail.com>
2024-09-26 17:42:32 +0500

    Ntra laravel

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import './hously/easy_background.js';
+import './hously/plugins.init.js';
+import './hously/app.js';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
